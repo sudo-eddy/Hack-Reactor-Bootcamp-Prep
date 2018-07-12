8833df61afeefaf8dba2d895fd5402e219395ec5
Eddy C <sudo.coding@protonmail.com>
2018-07-11 19:07:02 -0500

    Add excercise 4

---
+++ Module_1/Convert_to_object/004_convertObjectToArray2.js
@@ -0,0 +1,36 @@
+/*
+Write a function called "listAllValues" which returns an array of all the input object's values.
+
+Example input:
+{
+  name : 'Krysten',
+  age : 33,
+  hasPets : false
+}
+
+Function's return value (output):
+['Krysten', 33, false]
+
+Note that the input may have a different number of keys and values than the given sample.
+
+E.g. it should also handle an input like:
+{
+  a : 'a',
+  number : 11,
+  hungry : true,
+  grammyWins : 1
+}
+
+Function's return value (output):
+['a', 11, true, 1]
+
+Starter Code
+*/
+
+function listAllValues(obj) {
+  var showVal = [];
+  for(each in obj){
+    showVal.push(obj[each]);
+  }
+  return showVal;
+}
