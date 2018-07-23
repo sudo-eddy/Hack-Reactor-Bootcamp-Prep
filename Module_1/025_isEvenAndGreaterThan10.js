4beda2dac622e3d4a84842bc611155f64a438aab
Eddy C <sudo.coding@protonmail.com>
2018-07-23 13:37:37 -0500

    Add excercises 21-25

---
+++ Module_1/025_isEvenAndGreaterThan10.js
@@ -0,0 +1,10 @@
+/*
+Write a function called "isEvenAndGreaterThanTen".
+Given a number, "isEvenAndGreaterThanTen" returns whether it is both even and greater than 10.
+var output = isEvenAndGreaterThanTen(13);
+console.log(output); // --> false
+*/
+
+function isEvenAndGreaterThanTen(num) {
+  return (num > 10 && num % 2 === 0 ? true : false);
+}
