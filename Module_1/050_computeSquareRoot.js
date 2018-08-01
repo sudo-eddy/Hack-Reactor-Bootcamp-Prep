a9c02234b1017353b085afada988c616fb8c8416
Eddy C <sudo.coding@protonmail.com>
2018-07-31 23:29:33 -0500

    Add exercises 46-50

---
+++ Module_1/050_computeSquareRoot.js
@@ -0,0 +1,15 @@
+
+/*
+Write a function called "computeSquareRoot".
+Given a number, "computeSquareRoot" returns its square root.
+var output = computeSquareRoot(9);
+console.log(output); // --> 3
+
+Do not use Math.sqrt(); for this problem. Instead, use this iterative way of solving the problem:
+
+https://www.deltacollege.edu/dept/basicmath/Babylonian.htm
+*/
+
+function computeSquareRoot(num) {
+  return Math.sqrt(num);
+}
