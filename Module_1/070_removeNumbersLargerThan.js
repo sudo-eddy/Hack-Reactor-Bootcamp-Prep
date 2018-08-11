0f13076cc030f5f65f553aee4b4f0cc1788dc341
Eddy C <sudo.coding@protonmail.com>
2018-08-10 19:07:34 -0500

    Add exercises 66-70

---
+++ Module_1/070_removeNumbersLargerThan.js
@@ -0,0 +1,21 @@
+/*
+Write a function called "removeNumbersLargerThan".
+
+Given a number and an object, "removeNumbersLargerThan" removes any properties whose values are numbers greater than the given number.
+
+var obj = {
+  a: 8,
+  b: 2,
+  c: 'montana'
+}
+removeNumbersLargerThan(5, obj);
+console.log(obj); // --> { b: 2, c: 'montana' }
+*/
+
+function removeNumbersLargerThan(num, obj) {
+    for (let keyVal in obj) {
+      if(obj[keyVal] > num) {
+        delete obj[keyVal];
+      }
+    }
+}
